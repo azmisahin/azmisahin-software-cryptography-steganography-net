3f869cccd3e2eb29d98a3b85715e9f29da08ac25
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 15:27:26 +0300

    Char Extensions

---
+++ src/CryptoSteganography.Extensions.Char/Attribute.cs
@@ -0,0 +1,21 @@
+﻿using CryptoSteganography.Extensions;
+using System;
+
+namespace CryptoSteganography.Extensions.Char
+{
+    /// <summary>
+    /// Char Extension
+    /// </summary>
+    public static class Attribute
+    {
+        /// <summary>
+        /// Converts an char to a byte type.
+        /// </summary>
+        /// <param name="charItem"></param>
+        /// <returns></returns>
+        public static byte ToByte(this char charItem)
+        {
+            return Convert.ToByte(charItem);
+        }
+    }
+}
