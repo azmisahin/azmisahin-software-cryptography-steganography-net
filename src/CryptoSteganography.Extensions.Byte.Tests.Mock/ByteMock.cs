ed23cefc88482715780334fa4d3d9054bef52946
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 14:01:32 +0300

    Byte Mock Constants

---
+++ src/CryptoSteganography.Extensions.Byte.Tests.Mock/ByteMock.cs
@@ -0,0 +1,13 @@
+﻿namespace CryptoSteganography.Extensions.Byte.Tests.Mock
+{
+    /// <summary>
+    /// Byte Mock Const
+    /// </summary>
+    public static class BitMock
+    {
+        public static readonly byte ByteItem = 65;
+        public static readonly byte[] ByteArray = new byte[4] { 255, 254, 253, 252 };
+        public static readonly byte[] StringByteArray = new byte[19] { 67, 114, 121, 112, 116, 111, 83, 116, 101, 103, 97, 110, 111, 103, 114, 97, 112, 104, 121 };
+        public static readonly byte[] MergedByteArray = new byte[4] { 253, 252, 252, 253 };
+    }
+}
