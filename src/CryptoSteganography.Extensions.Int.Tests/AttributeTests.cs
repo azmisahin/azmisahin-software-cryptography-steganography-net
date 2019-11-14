080745d8b8658c8d863505f48883b556e89b3112
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 14:48:23 +0300

    Int Extension Tests

---
+++ src/CryptoSteganography.Extensions.Int.Tests/AttributeTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoSteganography.Extensions.Int;
+
+namespace CryptoSteganography.Extensions.Int.Tests
+{
+    [TestClass()]
+    public class AttributeTests
+    {
+        [TestMethod()]
+        public void ToByteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToByteArrayTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
