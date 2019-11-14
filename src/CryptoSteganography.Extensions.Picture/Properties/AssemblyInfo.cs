b33a0a5575cc1b90a9faeb9622ae93cccefa3a5d
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 15:53:46 +0300

    Picture Extension Project

---
+++ src/CryptoSteganography.Extensions.Picture/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CryptoSteganography.Extensions.Picture")]
+[assembly: AssemblyDescription("A condition in which the information contained in a readable data is not understood by the unwanted parties; Methods of converting a file, message, image, or video by hiding it in a file, message, image, or video.s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("CryptoSteganography.Extensions.Picture")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e9d5b399-6d84-4bb3-a8c4-32603fa72193")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
