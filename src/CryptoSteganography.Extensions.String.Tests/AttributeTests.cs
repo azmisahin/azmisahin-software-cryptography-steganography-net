e943d6f257c616337c3250a453e53eab08eed3f5
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 15:42:18 +0300

    String Extension Tests Project

---
+++ src/CryptoSteganography.Extensions.String.Tests/AttributeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoSteganography.Extensions.String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoSteganography.Extensions.String.Tests
+{
+    [TestClass()]
+    public class AttributeTests
+    {
+        [TestMethod()]
+        public void ToByteArrayTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/CryptoSteganography.Extensions.String.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CryptoSteganography.Extensions.String.Tests")]
+[assembly: AssemblyDescription("A condition in which the information contained in a readable data is not understood by the unwanted parties; Methods of converting a file, message, image, or video by hiding it in a file, message, image, or video.s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("CryptoSteganography.Extensions.String.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("84acb0d8-09d7-4363-83cd-485ae96c4b62")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
