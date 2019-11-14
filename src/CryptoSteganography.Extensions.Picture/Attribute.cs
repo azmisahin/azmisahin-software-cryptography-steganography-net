a33f321a82d0a960ee555d0725485bf887438762
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 15:55:12 +0300

    Picture Extension Attribute

---
+++ src/CryptoSteganography.Extensions.Picture/Attribute.cs
@@ -0,0 +1,11 @@
+﻿using CryptoSteganography.Extensions;
+
+namespace CryptoSteganography.Extensions.Picture
+{
+    /// <summary>
+    /// Picture Extension
+    /// </summary>
+    public static class Attribute
+    {
+    }
+}
