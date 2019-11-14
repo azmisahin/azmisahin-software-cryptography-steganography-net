8a9b7a4fba092d4ad1a86cbe386c1f81ba5e5c5a
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 15:59:34 +0300

    Picture Extension Tests

---
+++ src/CryptoSteganography.Extensions.Picture.Tests/AttributeTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoSteganography.Extensions.Picture;
+
+namespace CryptoSteganography.Extensions.Picture.Tests
+{
+    [TestClass()]
+    public class AttributeTests
+    {
+        [TestMethod()]
+        public void ToByteArrayTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/CryptoSteganography.Extensions.Picture.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CryptoSteganography.Extensions.Picture.Tests")]
+[assembly: AssemblyDescription("A condition in which the information contained in a readable data is not understood by the unwanted parties; Methods of converting a file, message, image, or video by hiding it in a file, message, image, or video.s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("CryptoSteganography.Extensions.Picture.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("82afbaa5-7c28-42a9-aa1e-f99e06ba20db")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
