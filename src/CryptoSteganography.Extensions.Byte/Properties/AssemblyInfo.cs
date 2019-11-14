352d48631f82a4213be22b8c466ee7c46c607846
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 14:03:11 +0300

    Byte Extension Project

---
+++ src/CryptoSteganography.Extensions.Byte/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CryptoSteganography.Extensions.Byte")]
+[assembly: AssemblyDescription("A condition in which the information contained in a readable data is not understood by the unwanted parties; Methods of converting a file, message, image, or video by hiding it in a file, message, image, or video.s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("CryptoSteganography.Extensions.Byte")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c6473fae-eed9-4aab-8ece-762c121757cd")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
