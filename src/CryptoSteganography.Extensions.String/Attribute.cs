e6565228e9ce90f144532dfc5c2ea905c1890ace
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 15:40:45 +0300

    String Extension Project

---
+++ src/CryptoSteganography.Extensions.String/Attribute.cs
@@ -0,0 +1,20 @@
+﻿using CryptoSteganography.Extensions;
+
+namespace CryptoSteganography.Extensions.String
+{
+    /// <summary>
+    /// String Extension
+    /// </summary>
+    public static class Attribute
+    {
+        /// <summary>
+        /// Converts string to byte array.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte[] ToByteArray(this string value)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(value);
+        }
+    }
+}
