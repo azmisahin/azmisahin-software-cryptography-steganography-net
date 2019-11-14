55d544f1df38fe460276653549b0569811af750f
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 15:57:26 +0300

    Picture ToByte Attribute

--- src/CryptoSteganography.Extensions.Picture/Attribute.cs
+++ src/CryptoSteganography.Extensions.Picture/Attribute.cs
@@ -1,4 +1,5 @@
 ﻿using CryptoSteganography.Extensions;
+using System.Drawing;
 
 namespace CryptoSteganography.Extensions.Picture
 {
@@ -7,5 +8,20 @@ namespace CryptoSteganography.Extensions.Picture
     /// </summary>
     public static class Attribute
     {
+        /// <summary>
+        /// Converts Color to byte.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte[] ToByteArray(this Color value)
+        {
+            byte[] result = new byte[4];
+            result[0] = value.A;
+            result[1] = value.R;
+            result[2] = value.G;
+            result[3] = value.B;
+
+            return result;
+        }
     }
 }
