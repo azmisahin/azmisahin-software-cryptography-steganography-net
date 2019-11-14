b9299f6652a21287f58eb577dd4f8360bddff556
Azmi SAHIN <azmisahin@outlook.com>
2019-11-14 15:32:45 +0300

    Char Extension Tests

---
+++ src/CryptoSteganography.Extensions.Char.Tests.Mock/CharMock.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptoSteganography.Extensions.Char.Tests.Mock
+{
+    public static class CharMock
+    {
+        public static readonly char CharItem = 'A';
+        public static readonly byte ByteItem = 65;
+    }
+}
---
+++ src/CryptoSteganography.Extensions.Char.Tests/AttributeTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoSteganography.Extensions.Char;
+using CryptoSteganography.Extensions.Char.Tests.Mock;
+
+namespace CryptoSteganography.Extensions.Char.Tests
+{
+    [TestClass()]
+    public class AttributeTests
+    {
+        [TestMethod()]
+        public void ToByteTest()
+        {
+            // Test Module
+            byte actual = CharMock.CharItem.ToByte();
+            byte expected = CharMock.ByteItem;
+
+            // Test Condition
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
