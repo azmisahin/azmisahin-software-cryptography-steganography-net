99f57501ba74e220da7865f4746f565e71eff421
Azmi SAHIN <azmisahin@outlook.com>
2019-11-15 11:10:47 +0300

    Base Proccesing Core

---
+++ src/Core/CryptoSteganography.Core/Proccesing.cs
@@ -0,0 +1,23 @@
+﻿namespace CryptoSteganography.Core
+{
+    /// <summary>
+    /// Core Proccesing
+    /// </summary>
+    public class Proccesing
+    {
+        /// <summary>
+        /// Matrix function used in concatenation and separation operations.
+        /// </summary>
+        /// <param name="row">byte row</param>
+        /// <param name="col">bit col</param>
+        /// <returns></returns>
+        public int F(int row, int col)
+        {
+            if (col < 2) throw new System.Exception(" 'col' must be greater than one.");
+            int area = row / col;
+            int fx = row - area;
+            if (fx == row) fx--;
+            return fx;
+        }
+    }
+}
