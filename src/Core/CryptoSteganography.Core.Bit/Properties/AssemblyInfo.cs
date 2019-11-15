ddf2cb1779749c63635b1443ecaf3549546f342b
Azmi SAHIN <azmisahin@outlook.com>
2019-11-15 12:02:56 +0300

    Core Bit Project

---
+++ src/Core/CryptoSteganography.Core.Bit/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CryptoSteganography.Core.Bit")]
+[assembly: AssemblyDescription("A condition in which the information contained in a readable data is not understood by the unwanted parties; Methods of converting a file, message, image, or video by hiding it in a file, message, image, or video.s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("CryptoSteganography.Core.Bit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e56687f6-262c-4739-8529-764e88b4d762")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
