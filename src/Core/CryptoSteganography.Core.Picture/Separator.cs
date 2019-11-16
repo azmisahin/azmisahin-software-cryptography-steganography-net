8b2af4df76653c49b6a4350f0fe69f71651675b3
Azmi SAHIN <azmisahin@outlook.com>
2019-11-16 21:40:31 +0300

    Core Picture Separator

---
+++ src/Core/CryptoSteganography.Core.Picture/Separator.cs
@@ -0,0 +1,46 @@
+﻿using CryptoSteganography.Extensions.Picture;
+using System.Drawing;
+
+namespace CryptoSteganography.Core.Picture
+{
+    /// <summary>
+    /// Picture Separator
+    /// </summary>
+    public class Separator
+    {
+        /// <summary>
+        /// Internal Color Item
+        /// </summary>
+        private Color _ColorItem { get; set; }
+        
+        /// <summary>
+        /// Color Item
+        /// </summary>
+        public Color ColorItem {  get { return _ColorItem; } }
+
+        /// <summary>
+        /// Byte Separator
+        /// </summary>
+        public Byte.Separator ByteSeparator { get; set; }
+
+        /// <summary>
+        /// Separator
+        /// </summary>
+        /// <param name="colorItem"></param>
+        public Separator(Color colorItem)
+        {
+            _ColorItem = colorItem;
+            byte[] byteArray = colorItem.ToByteArray();
+            ByteSeparator = new Byte.Separator(byteArray);
+        }
+
+        /// <summary>
+        /// Separate
+        /// </summary>
+        /// <returns></returns>
+        public byte SeparateByte()
+        {
+            return ByteSeparator.Separate();
+        }
+    }
+}
