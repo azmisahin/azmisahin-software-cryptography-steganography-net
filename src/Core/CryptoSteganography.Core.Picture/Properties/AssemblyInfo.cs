6f23f46f677322bbf1185de739cf5e2e0d4649e9
Azmi SAHIN <azmisahin@outlook.com>
2019-11-16 19:27:23 +0300

    Core Picture Project

---
+++ src/Core/CryptoSteganography.Core.Picture/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CryptoSteganography.Core.Picture")]
+[assembly: AssemblyDescription("A condition in which the information contained in a readable data is not understood by the unwanted parties; Methods of converting a file, message, image, or video by hiding it in a file, message, image, or video.s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("CryptoSteganography.Core.Picture")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d4e972f3-3e24-43a1-ac89-a00d8db766a7")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
