e389e767102c4a9f74b2284b4c0836a0940edeb7
Azmi SAHIN <azmisahin@outlook.com>
2019-11-16 21:45:08 +0300

    Picture Separator Test

---
+++ src/Tests/Core/CryptoSteganography.Core.Picture.Tests/SeparatorTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoSteganography.Core.Picture;
+using CryptoSteganography.Extensions.Picture.Tests.Mock;
+using CryptoSteganography.Extensions.Byte.Tests.Mock;
+
+namespace CryptoSteganography.Core.Picture.Tests
+{
+    [TestClass()]
+    public class SeparatorTests
+    {
+        [TestMethod()]
+        public void SeparatorTest()
+        {
+            // Instance
+            var instance = new Separator(PictureMock.SampleMergedColor);
+
+            // Test Condination
+            Assert.IsInstanceOfType(instance, typeof(Separator));
+        }
+
+        [TestMethod()]
+        public void SeparateByteTest()
+        {
+            // Instance
+            var instance = new Separator(PictureMock.SampleMergedColor);
+
+            // Test Modul
+            byte expected = ByteMock.ByteItem;
+            byte actual = instance.SeparateByte();
+
+            // Test Condination
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
