17f227cf9bff03021bafc877b02597cb77db7277
Azmi SAHIN <azmisahin@outlook.com>
2019-11-16 19:38:39 +0300

    Core Picture Tests

---
+++ src/Tests/Core/CryptoSteganography.Core.Picture.Tests/MergerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoSteganography.Core.Picture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoSteganography.Core.Picture.Tests
+{
+    [TestClass()]
+    public class MergerTests
+    {
+        [TestMethod()]
+        public void MergerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MergeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MergeTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/Tests/Core/CryptoSteganography.Core.Picture.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CryptoSteganography.Core.Picture.Tests")]
+[assembly: AssemblyDescription("A condition in which the information contained in a readable data is not understood by the unwanted parties; Methods of converting a file, message, image, or video by hiding it in a file, message, image, or video.s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("CryptoSteganography.Core.Picture.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a70cde23-914d-4759-ac26-6f1d1594c614")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
