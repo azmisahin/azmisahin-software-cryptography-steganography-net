45e480e0526a0ed5caee0cabba23370e67367b76
Azmi SAHIN <azmisahin@outlook.com>
2019-11-16 19:36:37 +0300

    Core Piture Merger

---
+++ src/Core/CryptoSteganography.Core.Picture/Merger.cs
@@ -0,0 +1,75 @@
+﻿using CryptoSteganography.Extensions.Char;
+using CryptoSteganography.Extensions.Picture;
+using System.Drawing;
+
+namespace CryptoSteganography.Core.Picture
+{
+    /// <summary>
+    /// Picture Merger
+    /// </summary>
+    public class Merger
+    {
+        /// <summary>
+        /// Internal Color Item
+        /// </summary>
+        private Color _ColorItem { get; set; }
+
+        /// <summary>
+        /// Merged Color
+        /// </summary>
+        public Color ColorItem { get { return _ColorItem; } }
+
+        /// <summary>
+        /// Byte Merger
+        /// </summary>
+        private Byte.Merger ByteMerger { get; set; }
+
+        /// <summary>
+        /// Merger Color.
+        /// </summary>
+        /// <param name="colorItem">Source Color</param>
+        public Merger(Color colorItem)
+        {
+            _ColorItem = colorItem;
+
+            ByteMerger = new Byte.Merger(colorItem.ToByteArray());
+        }
+
+        /// <summary>
+        /// Merge From Byte Item
+        /// </summary>
+        /// <param name="byteItem"></param>
+        /// <returns></returns>
+        public Color Merge(byte byteItem)
+        {
+            Color result;
+
+            byte[] byteArray = ByteMerger.Merge(byteItem);
+
+            int A, R, G, B;
+
+            A = byteArray[0];
+            R = byteArray[1];
+            G = byteArray[2];
+            B = byteArray[3];
+
+            result = Color.FromArgb(alpha: A, red: R, green: G, blue: B);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Merge From Char Item
+        /// </summary>
+        /// <param name="charItem"></param>
+        /// <returns></returns>
+        public Color Merge(char charItem)
+        {
+            byte byteItem = charItem.ToByte();
+
+            Color result = Merge(byteItem);
+
+            return result;
+        }
+    }
+}
