058894e78c0bfe86b1ccaa5b3b14ed93b2c524ac
Azmi SAHIN <azmisahin@outlook.com>
2019-11-17 14:48:26 +0300

    Picture Pixel Tests

---
+++ src/Core/CryptoSteganography.Core.Picture/Pixel.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+
+namespace CryptoSteganography.Core.Picture
+{
+    /// <summary>
+    /// Image Pixel
+    /// </summary>
+    public class Pixel
+    {
+        /// <summary>
+        /// Pixel ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Pixel X Horizontal Coordinate
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Pixel Y Vertical Coordinate
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Pixel Color
+        /// </summary>
+        public Color COLOR { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x">Horizontal Coordinate</param>
+        /// <param name="y">Vertical Coordinate</param>
+        /// <param name="color">ARGB Color</param>
+        public Pixel(int x, int y, Color color)
+        {
+            X = x;
+            Y = y;
+            COLOR = color;
+        }
+    }
+}
---
+++ src/Tests/Core/CryptoSteganography.Core.Picture.Tests/PixelTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoSteganography.Core.Picture;
+using CryptoSteganography.Extensions.Picture.Tests.Mock;
+
+namespace CryptoSteganography.Core.Picture.Tests
+{
+    [TestClass()]
+    public class PixelTests
+    {
+        [TestMethod()]
+        public void PixelTest()
+        {
+            // Instance
+            var instance = new Pixel(0, 0, PictureMock.SampleColor);
+
+            // Test Condination
+            Assert.IsInstanceOfType(instance, typeof(Pixel));
+        }
+    }
+}
