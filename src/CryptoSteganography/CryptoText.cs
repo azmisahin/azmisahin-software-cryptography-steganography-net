e0dab5f7d079379949b965521a9fa76072d2340b
Azmi SAHIN <azmisahin@outlook.com>
2019-11-24 01:11:24 +0300

    String Merge with  dictionary

--- src/CryptoSteganography/CryptoText.cs
+++ src/CryptoSteganography/CryptoText.cs
@@ -19,6 +19,57 @@ namespace CryptoSteganography
         /// <summary>
         /// 
         /// </summary>
+        private int _position = 2;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _sentence { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private string getWordsStartsWith(char item)
+        {
+            string word = item.ToString() + " ";
+            foreach (string str in words)
+            {
+                try
+                {
+                    char chr = str.Substring(_position - 1, 1)[0];
+
+                    bool IsLower = char.IsLower(item);
+
+                    char lower = char.ToLower(item);
+
+                    if (lower == chr)
+                    {
+                        if (!IsLower)
+                        {
+                            word = str.ToUpper() + " ";
+                        }
+                        else
+                        {
+                            word = str + " ";
+                        }
+                        return word;
+                    }
+                }
+                finally { }
+            }
+            return word;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Sentence { get { return _sentence; } }
+        
+        /// <summary>
+        /// 
+        /// </summary>
         /// <param name="_sourceFile"></param>
         public CryptoText(FileInfo _sourceFile)
         {
@@ -29,16 +80,21 @@ namespace CryptoSteganography
                 StreamReader streamReader = new StreamReader(_sourceFile.FullName);
                 var stringJson = streamReader.ReadToEnd();
                 words = stringJson.ToStringArray();
-            }            
+            }
         }
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="_mergeContent"></param>
-        public void Merge(string _mergeContent)
+        public string Merge(string _mergeContent)
         {
-            throw new NotImplementedException();
+            foreach (char item in _mergeContent)
+            {
+                string word = getWordsStartsWith(item);
+                _sentence += word;
+            }
+            return Sentence;
         }
 
         /// <summary>
