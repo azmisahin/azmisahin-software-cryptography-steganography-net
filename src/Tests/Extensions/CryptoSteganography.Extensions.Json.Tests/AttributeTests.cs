fe1c51e337c00fd515ffb958df12da382617cfcc
Azmi SAHIN <azmisahin@outlook.com>
2019-11-23 22:33:48 +0300

    Extension Json Tests

---
+++ src/Tests/Extensions/CryptoSteganography.Extensions.Json.Tests/AttributeTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoSteganography.Extensions.Json;
+
+namespace CryptoSteganography.Extensions.Json.Tests
+{
+    [TestClass()]
+    public class AttributeTests
+    {
+        [TestMethod()]
+        public void ToStringArrayTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/Tests/Extensions/CryptoSteganography.Extensions.Json.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("CryptoSteganography.Extensions.Json.Tests")]
+[assembly: AssemblyDescription("A condition in which the information contained in a readable data is not understood by the unwanted parties; Methods of converting a file, message, image, or video by hiding it in a file, message, image, or video.s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("CryptoSteganography.Extensions.Json.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e0980662-eadc-4031-b1f6-a12d01823feb")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
